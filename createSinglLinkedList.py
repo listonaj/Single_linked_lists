d0573f87d3655caf1c700052e3bf663353cede56
listonaj <jm.romain@outlook.com>
2022-06-03 12:26:37 -0700

    main file test

--- createSinglLinkedList.py
+++ createSinglLinkedList.py
@@ -100,21 +100,5 @@ class SLinkedList:
             self.head = None
             self.tail = None
 
-singlyLinkedList = SLinkedList()
-singlyLinkedList.insertSLL(44,6)
-print([node.value for node in singlyLinkedList]) 
-
-# node1 = Node(1)
-# node2 = Node(2)
-
-# singlyLinkedList.head = node1
-# singlyLinkedList.head.next = node2
-# singlyLinkedList.tail = node2
-
-singlyLinkedList.insertSLL(3,1)
-singlyLinkedList.insertSLL(4,1)
-print([node.value for node in singlyLinkedList]) 
-singlyLinkedList.insertSLL(5,3)
-print([node.value for node in singlyLinkedList]) 
 
 
