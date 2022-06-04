c98121886bb26db45625e6d580e70e8659b8d1d3
listonaj <jm.romain@outlook.com>
2022-06-03 18:37:10 -0700

    add some theorical chelp

--- linked_list.py
+++ linked_list.py
@@ -4,45 +4,20 @@ from SinglLinkedList import SLinkedList
 def main():
 
     print("this is the main function for the linked list")
-    singlLinkedList = SLinkedList()
-    singlLinkedList.insertSLL(44,6)
-    print([node.value for node in singlLinkedList]) 
+    my_linkedlist = SLinkedList()
 
+    # CREATE TWO NODE
     node1 = Node(1)
     node2 = Node(2)
 
-    singlLinkedList.head = node1
-    singlLinkedList.head.next = node2
-    singlLinkedList.tail = node2
+    # SPECIFY THE POSITION OF THE TWO NODES CREATED IN LINE 9
+    my_linkedlist.head = node1
+    my_linkedlist.next = node2
+    my_linkedlist.tail = node2
 
-    singlLinkedList.insertSLL(3,1)
-    singlLinkedList.insertSLL(4,1)
-    print([node.value for node in singlLinkedList]) 
-    singlLinkedList.insertSLL(5,3)
-    print([node.value for node in singlLinkedList]) 
+    
+    
 
-    singlLinkedList = SLinkedList()
-    # singlLinkedList.insertSLL(1, 1)
-    # singlLinkedList.insertSLL(2, 3)
-    # singlLinkedList.insertSLL(3, 1)
-    # singlLinkedList.insertSLL(4, 1)
-    # singlLinkedList.insertSLL(0, 0)
-    # singlLinkedList.insertSLL(7, -1)
-    # singlLinkedList.insertSLL(5, 6)
+    
 
-
-    # singlLinkedList.insertSLL(1,1)
-    # singlLinkedList.insertSLL(2,5)
-    # singlLinkedList.insertSLL(3,1)
-    # singlLinkedList.insertSLL(4,9)
-    # singlLinkedList.insertSLL(5,-1)
-    singlLinkedList.insertSLL(1, 1)
-    singlLinkedList.insertSLL(2, 1)
-    singlLinkedList.insertSLL(3, 1)
-    singlLinkedList.insertSLL(4, 1)
-    singlLinkedList.insertSLL(5,-1)
-
-    print([node.value for node in singlLinkedList]) 
-    singlLinkedList.deleteEntireSLL()
-    print([node.value for node in singlLinkedList]) 
-
+    
