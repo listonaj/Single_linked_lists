ad2120dfd4cf253bf0d88dfbca02e897ce0fc39d
listonaj <jm.romain@outlook.com>
2022-06-03 20:13:28 -0700

    solve issue by remove main function

--- linked_list.py
+++ linked_list.py
@@ -37,10 +37,63 @@ class SLinkedList:
                     newNode.next = nextNode
                     if tempNode == self.tail:
                         self.tail=newNode
-    
-my_list = SLinkedList()
-my_list.insertLL(1,1)
-my_list.insertLL(2,1)
+    # Traverse Singly Linked List
+    def traverseSLL(self):
+        if self.head is None:
+            print("The Singly Linked List does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    # Search for a node in Singly Linked List
+    def searchSLL(self, nodeValue):
+        if self.head is None:
+           return "The list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.value
+                node = node.next
+            return "The value does not exist in this list"
 
-for node in my_list:
-    print(node.value)
+    #  Delete a node from Singly Linked List
+    def deleteNode(self, location):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+    # Delete entire SLL
+    def deleteEntireSLL(self):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            self.head = None
+            self.tail = None
+    
--- my_linkedlist.py
+++ my_linkedlist.py
@@ -1,15 +1,10 @@
-from linked_list import SLinkedList
+from linked_list import *
 
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.next = None
+my_list = SLinkedList()
+my_list.insertLL(1,1)
+my_list.insertLL(2,1)
+my_list.insertLL(2,1)
+my_list.insertLL(2,1)
 
-def main ():
-
-    my_list = SLinkedList()
-    my_list.insertLL(1,1)
-    my_list.insertLL(2,1)
-
-    for node in my_list:
-        print(node.value)
+for node in my_list:
+    print(node.value)
