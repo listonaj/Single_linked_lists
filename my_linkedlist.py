c1079453cfd509ed4ba933043a8cdd55f0e12894
listonaj <jm.romain@outlook.com>
2022-06-03 22:48:29 -0700

    added some comments

--- my_linkedlist.py
+++ my_linkedlist.py
@@ -3,16 +3,16 @@ from linked_list import *
 # CREATE A NEW INSTANCE OF THE CLASS LINKED LIST
 my_list = SLinkedList()
 
-# add some values, each insert at the end of the list using the the 1 value 
+# INSERTING SOME ELEMENTS IN THE LIST 
 my_list.insertLL(1,1)
 my_list.insertLL(2,1)
 my_list.insertLL(3,1)
 my_list.insertLL(4,1)
 
-#I want to insert the value 23 at the begining of the linked list
+#I want to insert the value 23 at the beginning of the linked list
 my_list.insertLL(23,0)
 
-# we choose thelocation : value 15 inserted in at the 3rd position
+# we choose the position of the node: value 15 inserted in at the 3rd position
 my_list.insertLL(15,2)
 print("AFTER HAVING INSERTED SOME ELEMENTS IN THE LINKED LIST, I PRINT ITS CONTENT")
 print([node.value for node in my_list])
